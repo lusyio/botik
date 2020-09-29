897f9ce444c9697a5c98225a6bc5a59772a5c963
kamnev8823 <sapphire.s@bk.ru>
2020-09-29 17:04:41 +0300

    миграция

---
+++ database/migrations/2020_09_29_094946_create_importers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImportersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('importers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameRu');
+            $table->string('nameEn');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('importers');
+    }
+}
