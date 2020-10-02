a16cb20a3b6177f6f88fffcc216eab65e888ffaa
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-10-02 16:40:16 +0300

    Добавил возможность раскрывающейся таблицы, первая версия

--- resources/js/components/UI/AutoTable/AutoTable.tsx
+++ resources/js/components/UI/AutoTable/AutoTable.tsx
@@ -7,48 +7,128 @@ import BootstrapTable, {BootstrapTableProps} from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit';
 
+interface expandRow {
+    dataField: string,
+    text: string,
+    formatter?: string | React.FC
+}
+
 interface IAutoTable extends BootstrapTableProps {
     button: {
         link: string;
         text: string
-    }
+    } | undefined,
+    expandRowTable?: expandRow[] | undefined
 }
 
-const AutoTable: React.FC<IAutoTable> = ({data, columns, keyField, button}) => {
-    const {SearchBar} = Search;
-    return (
-        <ToolkitProvider
-            bootstrap4
-            keyField={keyField}
-            data={data}
-            columns={columns}
-            search
-        >
-            {props => (
-                <div>
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <SearchBar {...props.searchProps} />
+const AutoTable: React.FC<IAutoTable> =
+    ({
+         data,
+         columns,
+         keyField,
+         button,
+         expandRowTable = undefined
+     }) => {
+        const {SearchBar} = Search;
+
+        let expandRow = {}
+
+        if (expandRowTable) {
+            expandRow = {
+                expandColumnPosition: 'right',
+                renderer: renderer,
+                showExpandColumn: true,
+                expandHeaderColumnRenderer: expandHeaderColumnRenderer,
+                expandColumnRenderer: expandColumnRenderer
+            };
+        }
+
+        function renderer(row) {
+            const tableHead = expandRowTable
+                ?.map((item, index) => {
+                    return (
+                        <th key={`${item.text}-${index}`}>
+                            {item.text}
+                        </th>
+                    )
+                })
+            const tableBody = expandRowTable
+                ?.map((item, index) => {
+                    return (
+                        <td key={`${item.dataField}-${index}`}>
+                            {row[item.dataField]}
+                        </td>
+                    )
+                })
+            return (
+                <table className='table'>
+                    <thead>
+                    <tr>
+                        {tableHead}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        {tableBody}
+                    </tr>
+                    </tbody>
+                </table>
+            );
+        }
+
+        function expandHeaderColumnRenderer() {
+            return '';
+        }
+
+        function expandColumnRenderer({expanded}) {
+            if (expanded) {
+                return (
+                    <b>-</b>
+                );
+            }
+            return (
+                <b>...</b>
+            );
+        }
+
+        return (
+            <ToolkitProvider
+                bootstrap4
+                keyField={keyField}
+                data={data}
+                columns={columns}
+                search
+            >
+                {props => (
+                    <div>
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <SearchBar {...props.searchProps} />
+                            </div>
+                            {button
+                                ? <div className="col-lg-6">
+                                    <NavLink to={`/${button.link}`}>
+                                        {button.text}
+                                    </NavLink>
+                                </div>
+                                : null
+                            }
                         </div>
-                        <div className="col-lg-6">
-                            <NavLink to={`/${button.link}`}>
-                                {button.text}
-                            </NavLink>
+                        <div className='card'>
+                            <div className="card-body text-muted">
+                                <BootstrapTable
+                                    pagination={paginationFactory({})}
+                                    bordered={false}
+                                    // @ts-ignore
+                                    expandRow={expandRow}
+                                    {...props.baseProps}
+                                />
+                            </div>
                         </div>
                     </div>
-                    <div className='card'>
-                        <div className="card-body text-muted">
-                            <BootstrapTable
-                                pagination={paginationFactory({})}
-                                bordered={false}
-                                {...props.baseProps}
-                            />
-                        </div>
-                    </div>
-                </div>
-            )}
-        </ToolkitProvider>
-    )
-}
+                )}
+            </ToolkitProvider>
+        )
+    }
 
 export default AutoTable;
