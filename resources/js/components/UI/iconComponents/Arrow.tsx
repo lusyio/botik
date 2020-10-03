12372f786a96cd81a8dd9f1f42521a66541fb079
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-10-04 00:25:52 +0300

    поправил стили

--- resources/js/components/UI/iconComponents/Arrow.tsx
+++ resources/js/components/UI/iconComponents/Arrow.tsx
@@ -5,7 +5,7 @@ function SvgArrow(props: React.SVGProps<SVGSVGElement>) {
         <svg width="1em" height="1em" viewBox="0 0 10 6" fill="none" {...props}>
             <path
                 d="M9.814.186A.6.6 0 009.374 0H.626a.6.6 0 00-.44.186.601.601 0 00-.185.44.6.6 0 00.186.439L4.56 5.438c.124.124.27.186.44.186a.6.6 0 00.44-.186l4.374-4.375A.601.601 0 0010 .625a.602.602 0 00-.186-.44z"
-                fill="#3A405F"
+                fill="currentColor"
             />
         </svg>
     );
