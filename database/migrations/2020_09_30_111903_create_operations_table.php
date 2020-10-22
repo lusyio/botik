4e07bcf94636923f7b512f3df7a7325fbf9a8300
lusyio <mr-kelevras@yandex.ru>
2020-10-22 13:31:46 +0300

    Обновил миграции

--- database/migrations/2020_09_30_111903_create_operations_table.php
+++ database/migrations/2020_09_30_111903_create_operations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderItemsTable extends Migration
+class CreateOperationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateOrderItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('order_items', function (Blueprint $table) {
+        Schema::create('operations', function (Blueprint $table) {
             $table->id();
-            $table->integer('order_id');
-            $table->integer('product_id');
-            $table->integer('quantity');
-            $table->integer('price_cny');
+            $table->string('name');
+            $table->string('value');
+            $table->string('type');
+            $table->string('bank_id');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateOrderItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('order_items');
+        Schema::dropIfExists('operations');
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="referrer" content="no-referrer"/>
-    <title>Autolong</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+    <title>BOTIK</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png"/>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 </head>
 <body>
